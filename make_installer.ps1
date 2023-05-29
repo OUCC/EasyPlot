612289b8efec63d29a60013efe63aef59ca997dd
miyaji255 <miyayuu.since2018@gmail.com>
2023-05-29 21:33:02 +0900

    remove make_installer.ps1

--- make_installer.ps1
+++
@@ -1,1 +0,0 @@
-dotnet publish -f net7.0-windows10.0.19041.0 -c Release -p:RuntimeIdentifierOverride=win10-x64 -p:PackageCertificateThumbprintOverrride=5A0EB648A681DD7897A99AC075810F2C0F93F206 --self-contained true
